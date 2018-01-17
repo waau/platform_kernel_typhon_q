02613457289916bf7d22a7f6e4f9fc8bd5a30356
waau <echelon7t@gmail.com>
2018-01-17 08:14:08 -0500

    Remove some calls to trace_printk

--- fs/ext4/inline.c
+++ fs/ext4/inline.c
@@ -964,7 +964,6 @@ void ext4_show_inline_dir(struct inode *dir, struct buffer_head *bh,
 	struct ext4_dir_entry_2 *de = inline_start;
 	void *dlimit = inline_start + inline_size;
 
-	trace_printk("inode %lu\n", dir->i_ino);
 	offset = 0;
 	while ((void *)de < dlimit) {
 		de_len = ext4_rec_len_from_disk(de->rec_len, inline_size);
